c6592c0f82f4c292c793f5bd9ff7a235e137b454
Aaron Dack <adackbusiness@gmail.com>
2019-03-17 22:27:01 -0500

    Update

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -5,13 +5,11 @@ exports.onRenderBody = ({ setPostBodyComponents }, { pixelId }) => {
     console.error("Proof pixelId must be a string");
   }
 
-    return setPostBodyComponents([
-      <script
-        async
-        key={"gatsby-plugin-proof"}
-        dangerouslySetInnerHTML={{
-          __html: `https://cdn.useproof.com/proof.js?acc=${pixelId}`
-        }}
-      />
-    ]);
+  return setPostBodyComponents([
+    <script
+      async
+      key={"gatsby-plugin-proof"}
+      src={`https://cdn.useproof.com/proof.js?acc=${pixelId}`}
+    />
+  ]);
 };
